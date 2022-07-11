e37037dc59f7026ce882ee1239d0507eb005e212
wilsonibekason <wanbekgithub@gmail.com>
2022-07-10 19:14:23 -0700

    complete add heaer Tilte and react skelecton loader

---
+++ src/components/skeleton-loaders/HomePinLoader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+
+const  HomePinSkeletonLoader = () => {
+    return(
+        <section>
+            <h2>
+                <Skeleton duration={1} height={30} width={}/>
+            </h2>
+        </section>
+    )
+}
--- src/landing/partials/Header.jsx
+++ src/landing/partials/Header.jsx
@@ -47,7 +47,7 @@ function Header() {
                   <span>Sign up</span>
                   <svg className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
-                  </svg>                  
+                  </svg>                  skjyHyGco1OeGnTocv5jhiYGq8RCEIlRdzQ4jhf0BNxCpI7h6uu0ZvmmAMxqbBzj23G728LFPPz3pyc5vsQmvIHRfEoAw4B8CFusaQ2CXUF9kFEQHefN2hgcSph4cmvIqqdiPJ9BCK7ayEvSyw5AKjdJwxeC4fRUZLnLuDVtcOUwuj33qbd4
                 </Link>
               </li>
             </ul>
