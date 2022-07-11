58734ba1a81a8e3c5c46176f520236ca8ea9a501
wilsonibekason <wanbekgithub@gmail.com>
2022-07-11 08:10:43 -0700

    complete add dependency

--- src/landing/partials/Features.jsx
+++ src/landing/partials/Features.jsx
@@ -34,7 +34,7 @@ function Features() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h1 className="h2 mb-4">Explore the solutions</h1>
-            <p className="text-xl text-gray-600">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat.</p>
+            <p className="text-xl text-gray-600">See Contribute Fun Experience Privacy Security Popularity Advertise.</p>
           </div>
 
           {/* Section content */}
