e0dd9639b7699e996407ceaa7c1115eb75f51a69
wilsonibekason <wanbekgithub@gmail.com>
2022-07-11 14:56:15 -0700

    complete add custom

--- src/landing/partials/Testimonials.jsx
+++ src/landing/partials/Testimonials.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import TestimonialImage from "../images/testimonial.jpg";
-
+const gradientReused = "bg-clip-text text-transparent";
 function Testimonials() {
   return (
     <section className="relative">
@@ -45,11 +45,17 @@ function Testimonials() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h2 className="h2 mb-4">
-              Trusted by over 20,000 companies all over the world
+              Trusted by many organization and clients
             </h2>
             <p className="text-xl text-gray-600" data-aos="zoom-y-out">
-              Arcu cursus vitae congue mauris rhoncus viverra nibh cras pulvinar
-              mattis blandit libero cursus mattis.
+              <span
+                className={`${gradientReused} bg-gradient-to-l from-red-200 to-gray-200`}
+              >
+                {" "}
+                Shareme
+              </span>
+              is trusted by many organisations and clients and more than we can
+              imagine
             </p>
           </div>
 
