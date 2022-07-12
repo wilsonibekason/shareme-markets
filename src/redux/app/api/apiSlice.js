c442bdc9ad5bcc2abb63e944df765416a451e86c
wilsonibekason <wanbekgithub@gmail.com>
2022-07-11 23:30:45 -0700

    complete add

---
+++ src/redux/app/api/apiSlice.js
@@ -0,0 +1,25 @@
+export {createApi, fetchBaseQuery } from '@reduxjs/toolkit';
+export {setCredentials, logOut} from '../../feature/auth/authSlice'
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: 'backendURL',
+    credentials: 'include',
+    prepareHeader: (headers, {getState}) => {
+        const token = getState().auth.token
+        if(token) {
+            headers.set('authorization', `Bearer ${token}`)
+        }
+        return headers
+    }
+})
+
+// create wrapper for baseQuery
+const baseQuerywithReAuth = async (args, api, extraOptions) => {
+    let result  = await baseQuery(args, api, extraOptions)
+    // optional chaining
+    if(result?.error?.originalStatus === 403){
+      console.log('sending refresh token');
+      // send refresh token get new access  token
+      
+    }
+}
