86614895776c9a93c1c47c87f39d95a0e5a420af
wilsonibekason <wanbekgithub@gmail.com>
2022-07-12 19:55:01 -0700

    complete create redux store for login

--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -1,5 +1,19 @@
 import {configureStore} from '@reduxjs/toolkit';
 import { apiSlice } from './api/apiSlice';
+import authReducer from '../feature/auth/authSlice'
 
+export const store = configureStore({
+    reducer: {
+        // ...authReducer,
+        // auth: {
+        //     ...authReducer.auth,
+        // }
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        auth: authReducer,
+
+    },
+    middleware: getDefaultMiddleware =>  getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true,
+})
 
 authReducer
