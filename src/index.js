bc43620abafbdd8690ce3a557ef8620fe8702e49
wilsonibekason <wanbekgithub@gmail.com>
2022-07-12 20:01:33 -0700

    complete wrap redux add to root app

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <React.StrictMode>
-      <App />
+      <Provider store={Store}>
+<App />
+      </Provider>
+      
     </React.StrictMode>
   </Router>
 );
