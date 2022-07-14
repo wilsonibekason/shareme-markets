1f6fa2dd98498681990f66264038eeca95575eb4
wilsonibekason <wanbekgithub@gmail.com>
2022-07-14 14:11:26 +0100

    complete uncomment ApiSlice

--- src/redux/app/api/apiSlice.js
+++ src/redux/app/api/apiSlice.js
@@ -1,41 +1,41 @@
-// export {createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-// export {setCredentials, logOut} from '../../feature/auth/authSlice'
+export {createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+export {setCredentials, logOut} from '../../feature/auth/authSlice'
 
-// const baseQuery = fetchBaseQuery({
-//     baseUrl: 'backendURL',
-//     credentials: 'include',
-//     prepareHeader: (headers, {getState}) => {
-//         const token = getState().auth.token
-//         if(token) {
-//             headers.set('authorization', `Bearer ${token}`)
-//         }
-//         return headers
-//     }
-// })
+const baseQuery = fetchBaseQuery({
+    baseUrl: 'backendURL',
+    credentials: 'include',
+    prepareHeader: (headers, {getState}) => {
+        const token = getState().auth.token
+        if(token) {
+            headers.set('authorization', `Bearer ${token}`)
+        }
+        return headers
+    }
+})
 
-// // create wrapper for baseQuery
-// const baseQuerywithReAuth = async (args, api, extraOptions) => {
-//     let result  = await baseQuery(args, api, extraOptions)
-//     // optional chaining
-//     if(result?.error?.originalStatus === 403){
-//       console.log('sending refresh token');
-//       // send refresh token get new access  token
-//       const refreshResults = await baseQuery('/refresh', api, extraOptions)
-//       console.log(refreshResults)
-//     if(refreshResults?.data) {
-//        const user = api.getState().auth.user
-//        // store the API token here
-//        api.dispatch(setCredentials({...refreshResults?.data, user}));
-//        // retry the original query with new access token
-//        result = await baseQuery(args, api, extraOptions)
-//     }  else{
-//       api.dispatch(logOut);
-//     }
-//   }
-//   return result;
-// }
-// // exporting the apiSlice
-// export  const apiSlice = createApi({
-//   baseQuery: baseQueryWithResults,
-//   endPoints: builder => ({})
-// })
+// create wrapper for baseQuery
+const baseQuerywithReAuth = async (args, api, extraOptions) => {
+    let result  = await baseQuery(args, api, extraOptions)
+    // optional chaining
+    if(result?.error?.originalStatus === 403){
+      console.log('sending refresh token');
+      // send refresh token get new access  token
+      const refreshResults = await baseQuery('/refresh', api, extraOptions)
+      console.log(refreshResults)
+    if(refreshResults?.data) {
+       const user = api.getState().auth.user
+       // store the API token here 
+       api.dispatch(setCredentials({...refreshResults?.data, user}));
+       // retry the original query with new access token
+       result = await baseQuery(args, api, extraOptions)
+    }  else{
+      api.dispatch(logOut);
+    }
+  }
+  return result;
+}
+// exporting the apiSlice
+export  const apiSlice = createApi({
+  baseQuery: baseQueryWithResults,
+  endPoints: builder => ({})
+})
