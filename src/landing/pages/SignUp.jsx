26ed4be028b2fa7d94db0f578dbdace8432d20c3
wilsonibekason <wanbekgithub@gmail.com>
2022-07-16 16:51:06 +0100

    complete signup UI

--- src/landing/pages/SignUp.jsx
+++ src/landing/pages/SignUp.jsx
@@ -16,7 +16,7 @@ function SignUp() {
             <div className="pt-32 pb-12 md:pt-40 md:pb-20">
               {/* Page header */}
               <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-                <h1 className="h1">
+                <h1 className="text-sm lg:text-lg">
                   This is your first step to unlimited experiences
                 </h1>
               </div>
@@ -35,7 +35,7 @@ function SignUp() {
                       <input
                         id="name"
                         type="text"
-                        className="form-input w-full text-gray-800"
+                        className="form-input w-full text-gray-800 rounded-full"
                         placeholder="Enter your name"
                         required
                       />
@@ -52,7 +52,7 @@ function SignUp() {
                       <input
                         id="email"
                         type="email"
-                        className="form-input w-full text-gray-800"
+                        className="form-input w-full text-gray-800 rounded-full"
                         placeholder="Enter your email address"
                         required
                       />
@@ -69,7 +69,7 @@ function SignUp() {
                       <input
                         id="password"
                         type="password"
-                        className="form-input w-full text-gray-800"
+                        className="form-input w-full text-gray-800 rounded-full"
                         placeholder="Enter your password"
                         required
                       />
@@ -77,18 +77,18 @@ function SignUp() {
                   </div>
                   <div className="flex flex-wrap -mx-3 mt-6">
                     <div className="w-full px-3">
-                      <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">
+                      <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full text-sm rounded-full">
                         Sign up
                       </button>
                     </div>
                   </div>
-                  <div className="text-sm text-gray-500 text-center mt-3">
+                  <div className="text-[.65rem] lg:text-sm  text-gray-500 text-center mt-3">
                     By creating an account, you agree to the{" "}
                     <a className="underline" href="#0">
                       terms & conditions
                     </a>
                     , and our{" "}
-                    <a className="underline" href="#0">
+                    <a className="underline text-[.65rem] lg:text-sm  " href="#0">
                       privacy policy
                     </a>
                     .
@@ -99,32 +99,17 @@ function SignUp() {
                     className="border-t border-gray-300 flex-grow mr-3"
                     aria-hidden="true"
                   ></div>
-                  <div className="text-gray-600 italic">Or</div>
+                  <div className="text-gray-600 italic text-sm">Or</div>
                   <div
                     className="border-t border-gray-300 flex-grow ml-3"
                     aria-hidden="true"
                   ></div>
                 </div>
                 <form>
-                  <div className="flex flex-wrap -mx-3 mb-3">
-                    <div className="w-full px-3">
-                      <button className="btn px-0 text-white bg-gray-900 hover:bg-gray-800 w-full relative flex items-center">
-                        <svg
-                          className="w-4 h-4 fill-current text-white opacity-75 flex-shrink-0 mx-4"
-                          viewBox="0 0 16 16"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path d="M7.95 0C3.578 0 0 3.578 0 7.95c0 3.479 2.286 6.46 5.466 7.553.397.1.497-.199.497-.397v-1.392c-2.187.497-2.683-.993-2.683-.993-.398-.895-.895-1.193-.895-1.193-.696-.497.1-.497.1-.497.795.1 1.192.795 1.192.795.696 1.292 1.888.895 2.286.696.1-.497.298-.895.497-1.093-1.79-.2-3.578-.895-3.578-3.975 0-.895.298-1.59.795-2.087-.1-.2-.397-.994.1-2.087 0 0 .695-.2 2.186.795a6.408 6.408 0 011.987-.299c.696 0 1.392.1 1.988.299 1.49-.994 2.186-.795 2.186-.795.398 1.093.199 1.888.1 2.087.496.596.795 1.291.795 2.087 0 3.08-1.889 3.677-3.677 3.875.298.398.596.895.596 1.59v2.187c0 .198.1.497.596.397C13.714 14.41 16 11.43 16 7.95 15.9 3.578 12.323 0 7.95 0z" />
-                        </svg>
-                        <span className="flex-auto pl-16 pr-8 -ml-16">
-                          Continue with GitHub
-                        </span>
-                      </button>
-                    </div>
-                  </div>
+                 
                   <div className="flex flex-wrap -mx-3">
                     <div className="w-full px-3">
-                      <button className="btn px-0 text-white bg-red-600 hover:bg-red-700 w-full relative flex items-center">
+                      <button className="btn px-0 text-white bg-red-600 hover:bg-red-700 w-full relative flex items-center rounded-full">
                         <svg
                           className="w-4 h-4 fill-current text-white opacity-75 flex-shrink-0 mx-4"
                           viewBox="0 0 16 16"
@@ -132,18 +117,18 @@ function SignUp() {
                         >
                           <path d="M7.9 7v2.4H12c-.2 1-1.2 3-4 3-2.4 0-4.3-2-4.3-4.4 0-2.4 2-4.4 4.3-4.4 1.4 0 2.3.6 2.8 1.1l1.9-1.8C11.5 1.7 9.9 1 8 1 4.1 1 1 4.1 1 8s3.1 7 7 7c4 0 6.7-2.8 6.7-6.8 0-.5 0-.8-.1-1.2H7.9z" />
                         </svg>
-                        <span className="flex-auto pl-16 pr-8 -ml-16">
+                        <span className="flex-auto pl-16 pr-8 -ml-16 text-[.7rem] lg:text-sm">
                           Continue with Google
                         </span>
                       </button>
                     </div>
                   </div>
                 </form>
-                <div className="text-gray-600 text-center mt-6">
-                  Already using Simple?{" "}
+                <div className="text-gray-600 text-center mt-6 text-sm">
+                  Already using Shareme?{" "}
                   <Link
                     to="/signin"
-                    className="text-blue-600 hover:underline transition duration-150 ease-in-out"
+                    className="text-blue-600 hover:underline transition duration-150 ease-in-out text-sm"
                   >
                     Sign in
                   </Link>
