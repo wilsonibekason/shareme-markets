1df0b9509deb71005f18adafe43d1410cc67cc69
wilsonibekason <wanbekgithub@gmail.com>
2022-07-16 19:08:38 +0100

    complete footer UI

--- src/landing/partials/Footer.jsx
+++ src/landing/partials/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import LogoSvg from "../../assets/logo.png";
 function Footer() {
   return (
     <footer>
@@ -12,33 +12,7 @@ function Footer() {
             <div className="mb-2">
               {/* Logo */}
               <Link to="/" className="inline-block" aria-label="Cruip">
-                <svg
-                  className="w-8 h-8"
-                  viewBox="0 0 32 32"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <defs>
-                    <radialGradient
-                      cx="21.152%"
-                      cy="86.063%"
-                      fx="21.152%"
-                      fy="86.063%"
-                      r="79.941%"
-                      id="footer-logo"
-                    >
-                      <stop stopColor="#4FD1C5" offset="0%" />
-                      <stop stopColor="#81E6D9" offset="25.871%" />
-                      <stop stopColor="#338CF5" offset="100%" />
-                    </radialGradient>
-                  </defs>
-                  <rect
-                    width="32"
-                    height="32"
-                    rx="16"
-                    fill="url(#footer-logo)"
-                    fillRule="nonzero"
-                  />
-                </svg>
+                <img src={LogoSvg} alt="shareme-logo" />
               </Link>
             </div>
             <div className="text-sm text-gray-600">
